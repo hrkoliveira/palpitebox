a4a4a6f96941dd636448de181b4cd3f617fcce38
Herik Oliveira <herikoliveira@DESKTOP-K99557S.localdomain>
2022-05-24 22:42:36 -0300

    atualizado Whatsapp

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,10 @@ const Footer = () => {
       <div className='container mx-auto text-center font-bold text-white'>
       © Projeto desenvolvido por: Herik Oliveira /{' '}
         <a className='hover:underline' href='https://www.linkedin.com/in/herik-oliveira/' target='_blank'>Linkedin</a> /{' '}
-        <a  className='hover:underline' href='https://github.com/hrkoliveira' target='_blank'>Github </a>
+        <a  className='hover:underline' href='https://github.com/hrkoliveira' target='_blank'>Github </a> /{' '}
+        <a  className='hover:underline' href='https://api.whatsapp.com/send?phone=92999053105&text=Entrar%20em%20contato' target='_blank'>Whatsapp </a>
+      </div>
+      <div>
       </div>
     </div>
   )
